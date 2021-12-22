827b6cdfb4d14736a5c7ccba3e8115850ee0fda5
sequws <workspaceshare@gmail.com>
2021-12-22 22:17:39 +0100

    Add GoBack navigation StatementView

--- TradingSolution/StatementModule/ViewModels/StatementViewModel.cs
+++ TradingSolution/StatementModule/ViewModels/StatementViewModel.cs
@@ -1,16 +1,33 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
 
 namespace StatementModule.ViewModels
 {
     public class StatementViewModel : BindableBase
     {
-        public StatementViewModel()
-        {
+        public string Info { get; set; } = "Statement info";
+        private readonly IRegionManager _regionManager;
+        public ICommand GoBackCommand { get; set; }
 
+        public StatementViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+            GoBackCommand = new DelegateCommand(GoBack, CanGoBack);
+        }
+
+        private bool CanGoBack()
+        {
+            return _regionManager.Regions["ContentRegion"].NavigationService.Journal.CanGoBack;/*or CanGoForward */
+        }
+
+        private void GoBack()
+        {
+            _regionManager.Regions["ContentRegion"].NavigationService.Journal.GoBack();//
         }
     }
 }
