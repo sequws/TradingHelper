7391d097e5741baf1edf7d5c3e48813267f73863
sequws <workspaceshare@gmail.com>
2021-12-27 22:42:54 +0100

    Add classes for JournalModule

---
+++ TradingSolution/JournalModule/Modules/Strategy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JournalModule.Modules
+{
+    public class Strategy
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Rule> Rules { get; set; }
+    }
+
+    public class Rule
+    {
+        public string Description { get; set; }
+        public bool Satisfied { get; set; }
+    }
+
+    public class TradingDay
+    {
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public List<TransactionHistory> Transactions { get; set; }
+    }
+
+    public enum TransactionStatus
+    {
+        Open,
+        Close,
+        Pending
+    }
+
+    public class TransactionHistory
+    {
+        public DateTime Date { get; set; }
+        public TransactionStatus Status { get; set; }
+    }
+}
