c0de6bb4b66197642db889e51eab6167d4e95aba
sequws <workspaceshare@gmail.com>
2022-01-06 22:49:23 +0100

    Add CandleOhcl model

---
+++ TradingSolution/AnalysisModule/Models/OhlcCandle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalysisModule.Models
+{
+    public class CandleOhlc
+    {
+        double Open { get; set; }
+        double High { get; set; }
+        double Low { get; set; }
+        double Close { get; set; }
+        DateTime dateTime { get; set; }
+    }
+}
