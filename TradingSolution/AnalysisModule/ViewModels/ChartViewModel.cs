31090804c58d96a161d7806e0b0ab503f32dd380
sequws <workspaceshare@gmail.com>
2022-01-22 01:26:04 +0100

    Add ChartView

---
+++ TradingSolution/AnalysisModule/ViewModels/ChartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnalysisModule.ViewModels
+{
+    public class ChartViewModel : BindableBase
+    {
+        public ChartViewModel()
+        {
+
+        }
+    }
+}
---
+++ TradingSolution/AnalysisModule/Views/ChartView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AnalysisModule.Views
+{
+    /// <summary>
+    /// Interaction logic for ChartView
+    /// </summary>
+    public partial class ChartView : UserControl
+    {
+        public ChartView()
+        {
+            InitializeComponent();
+        }
+    }
+}
